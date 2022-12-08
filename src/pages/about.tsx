cb0a0cb7b5e1c7fa6676377a12968586e592ee25
JhonatanWCL99 <wilsoncoyoleon@gmail.com>
2022-12-08 15:42:37 -0400

    Con mi Foto :v

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
 import mirandaImage from "../assets/jose-luis-miranda.jpg";
-import coyoImage from "../assets/jose-luis-miranda.jpg";
+import coyoImage from "../assets/jhonatan-coyo-leon.jpg";
 import React from "react";
-import {useTranslation} from "next-i18next";
+import { useTranslation } from "next-i18next";
 import Link from "next/link";
 
 function About(props: any) {
-    const {t, i18n} = useTranslation();
+    const { t, i18n } = useTranslation();
     return <div className={'h-screen'}>
         <div className={'grid grid-cols-2 justify-items-center mt-20 gap-10'}>
 
             <div className={'flex flex-col items-center w-1/2'}>
                 <div className={'bg-white rounded-md w-48 p-3'}>
-                    <Image src={mirandaImage} alt="" className={'aspect-square'}/>
+                    <Image src={mirandaImage} alt="" className={'aspect-square'} />
                 </div>
                 <span className={'block text-center text-white font-semibold mt-2'}>{t('JOSE LUIS MIRANDA')}</span>
                 <div className={'flex justify-center gap-5'}>
@@ -33,7 +33,7 @@ function About(props: any) {
 
             <div className={'flex flex-col items-center w-1/2'}>
                 <div className={'bg-white rounded-md w-48 p-3'}>
-                    <Image src={coyoImage} alt="" className={'aspect-square'}/>
+                    <Image src={coyoImage} alt="" className={'aspect-square'} />
                 </div>
                 <span className={'block text-center text-white font-semibold mt-2'}>{t('JHONATAN WILSON COYO')}</span>
                 <div className={'flex justify-center gap-5'}>
